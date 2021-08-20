7814c3d1131084bd28d7cb2c846b987774051ded
Eric <eric.m.lombardo@gmail.com>
2021-08-20 12:42:46 -0400

    Built async fetch request with thunk

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,15 @@
-// write and export your action creator function here
+const fetchCats = () => {
+  return (dispatch) => {
+    dispatch({ 
+      type: 'LOADING_CATS' 
+    })
+    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+    .then(resp => resp.json())
+    .then(cats => dispatch({ 
+      type: 'ADD_CATS', 
+      cats: cats.images 
+    }))
+  }
+}
+
+export { fetchCats }
